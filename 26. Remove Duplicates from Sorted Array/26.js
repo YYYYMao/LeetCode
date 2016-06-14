e2ddb2c12940f0de22472642d2f77ffbe32ac516
MaoYing <maoying0980@gmail.com>
2016-06-14 11:40:23 +0800

    add 70

--- 26. Remove Duplicates from Sorted Array/26.js
+++ 26. Remove Duplicates from Sorted Array/26.js
@@ -8,7 +8,7 @@ var removeDuplicates = function(nums) {
     if(nums == [])	 return 0;
     if(nums.length == 1)	 return 1;
     tmp = nums[0];
-    for(i = 0 , j = i+1 ; i < nums.length ,j < nums.length;j++){		
+    for(i = 0 , j = i+1 ;j < nums.length;j++){		
          if(tmp != nums[j] ){
                nums[i+1] = nums[j];
                n++;
