e01bad3ecf12685a75dabdb8660c132bddcb4012
MaoYing <maoying0980@gmail.com>
2016-06-15 17:13:21 +0800

    add 283

---
+++ 283. Move Zeroes/283.js
@@ -0,0 +1,26 @@
+// 283. Move Zeroes
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+// Note:
+// You must do this in-place without making a copy of the array.
+// Minimize the total number of operations.
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    var j ;	
+    for(var i = 0 , j = 0; j <  nums.length; i++){
+    	if(i >= nums.length){
+    	    nums[j] = 0;
+                 j++;	
+    	}else{
+    	    if(nums[i] !== 0){
+                   nums[j] = nums[i];
+                   j++;
+    	    }	
+    	}
+    }
+};
