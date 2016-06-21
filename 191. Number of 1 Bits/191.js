11d042043be53ebc96c067726f4548763233d908
MaoYing <maoying0980@gmail.com>
2016-06-21 10:10:10 +0800

    add 191

---
+++ 191. Number of 1 Bits/191.js
@@ -0,0 +1,16 @@
+// 191. Number of 1 Bits
+// Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+// For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    var c = 0;	
+    while(n >0){
+       c =c + (n&1);
+       n = parseInt(n/2);
+    }
+    return c;
+};
