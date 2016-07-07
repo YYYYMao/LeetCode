e5cde521af7ff342c6e3361933bac3d97e2d799d
MaoYing <maoying0980@gmail.com>
2016-07-07 10:47:53 +0800

    219. Contains Duplicate II/

---
+++ 219. Contains Duplicate II/219.js
@@ -0,0 +1,23 @@
+219. Contains Duplicate II
+Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array 
+such that nums[i] = nums[j] and the difference between i and j is at most k.
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    var n = [];
+    for(var i = 0 ; i < nums.length ; i++){
+    	if(n[nums[i]] == undefined){
+    		n[nums[i]] = i;
+    	}else{
+    		if((i - n[nums[i]]) <=k){
+    			return true;
+    		}else{
+    		    n[nums[i]] = i;	
+    		}
+    	}
+    }
+    return false;
+};
