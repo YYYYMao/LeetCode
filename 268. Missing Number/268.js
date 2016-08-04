730f46426df117b64d210ec08322538081123421
MaoYing <maoying0980@gmail.com>
2016-08-04 15:35:10 +0800

    268. Missing Number/

---
+++ 268. Missing Number/268.js
@@ -0,0 +1,20 @@
+268. Missing Number
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+For example,
+Given nums = [0, 1, 3] return 2.
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    var a = 0;
+    for(var i = 0 ; i < nums.length ; i++){
+          a ^= (i+1) ^ nums[i];
+    }
+    return a;
+};
