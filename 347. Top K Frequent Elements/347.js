91e6d5b74d3f8d5cf5b328dec5d0af6c00bb6182
MaoYing <maoying0980@gmail.com>
2016-08-16 15:28:50 +0800

    347. Top K Frequent Elements/

---
+++ 347. Top K Frequent Elements/347.js
@@ -0,0 +1,39 @@
+347. Top K Frequent Elements
+Given a non-empty array of integers, return the k most frequent elements.
+
+For example,
+Given [1,1,1,2,2,3] and k = 2, return [1,2].
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    var arr = [];
+    var sort = [];
+    var result = [];
+    for(var i = 0 ; i < nums.length ; i++){
+         if(arr[nums[i]] == undefined) arr[nums[i]] = 0;
+         arr[nums[i]]++;
+    }
+    for(i in arr){
+       if(sort[arr[i]] == undefined) sort[arr[i]] = []; 	
+       sort[arr[i]].push(i);
+    }console.log(sort);
+    for(i = sort.length-1,j = 0 ; j < k ;i--){
+    	console.log(i);
+             if(sort[i] !== undefined){
+                for(var k = 0 ; k < sort[i].length ; k++){
+                  console.log(sort[i].length);	
+                  result.push(parseInt(sort[i][k]));	
+                  j++;	
+                  console.log(j);
+                }	
+             }
+    }
+    return result;
+};
