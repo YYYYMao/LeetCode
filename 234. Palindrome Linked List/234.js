9584fd8819fac9c7b05f9663fe5cb9b947c83a5e
MaoYing <maoying0980@gmail.com>
2018-06-07 10:16:12 +0800

    234. Palindrome Linked List/

---
+++ 234. Palindrome Linked List/234.js
@@ -0,0 +1,35 @@
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+Follow up:
+Could you do it in O(n) time and O(1) space?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    var cur = head;
+    var s = "";
+    var r = "";
+    while(cur){
+        s += cur.val;
+        r =  cur.val+r
+        cur = cur.next;
+    }
+    return s == r; 
+};
