f58c5042f1dec9559d0a8c5c731ef7483fe51fe3
MaoYing <maoying0980@gmail.com>
2018-06-15 16:00:10 +0800

    535. Encode and Decode TinyURL/

---
+++ 535. Encode and Decode TinyURL/535.js
@@ -0,0 +1,34 @@
+Note: This is a companion problem to the System Design problem: Design TinyURL.
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+
+Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. 
+You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var table = {};
+var encode = function(longUrl) {
+   var str = "abcde12345" 
+   var shortUrl = str[Math.random()*10] 
+   table[shortUrl] = longUrl
+   return  shortUrl;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+    return table[shortUrl];
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
